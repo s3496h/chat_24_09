b00099cf9ea7934e5a433d597ae34a97e67df98c
s3496h <s3496h@gmail.com>
2024-09-06 09:11:32 +0900

    Ajax,SSE 채팅방 구현 작업 4, ChatMessage 클래스 관련 테스트

---
+++ src/main/java/org/koreait/exit/chat_24_09/ChatController.java
@@ -0,0 +1,15 @@
+package org.koreait.exit.chat_24_09;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    ChatMessage message = new ChatMessage("홍길동", "안녕");
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public RsData<ChatMessage> writeMessage() {
+        return new RsData<>("S-1", "메세지가 작성됨", message);
+    }
+}
---
+++ src/main/java/org/koreait/exit/chat_24_09/ChatMessage.java
@@ -0,0 +1,20 @@
+package org.koreait.exit.chat_24_09;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ChatMessage {
+    private long id;
+    private LocalDateTime createDate;
+    private String authorName;
+    private String content;
+
+    public ChatMessage(String authorName, String content) {
+        this(1, LocalDateTime.now(), authorName, content);
+    }
+
+}
